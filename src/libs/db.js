e4f666f383950c7e0fa695ebe08b02ba5d48062d
yangming <472102644@qq.com>
2018-01-03 23:07:58 +0800

    更改界面排版

--- src/libs/db.js
+++ src/libs/db.js
@@ -4,96 +4,107 @@ import utils from './utils'
 
 
 export default {
-    user: null,
-    
-    openid: 0,
-    
-    setTableContent () {
-        const Content = AV.Object.extend('content')
-        return new Content()
-    },
-    
-    setComment () {
-        const Comment = AV.Object.extend('comment')
-        return new Comment()
-    },
-    
-    commonField () {
-        return {
-            user: this.user,
-            openid: this.openid
-        }
-    },
-    
-    getOneTalk (id) {
-        return AV.Object.createWithoutData('content', id)
-    },
-    
-    queryBy (dataType, isOwn = true, limit, limitCondition) {
-        let queryContent
-        let queryContent2
-        let query
-        
-        if (dataType) {
-            queryContent = new AV.Query('content')
-            queryContent.equalTo('dataType', dataType)
-            query = queryContent
-        }
-        
-        if (isOwn) {
-            queryContent2 = new AV.Query('content')
-            queryContent2.equalTo('openid', this.openid)
-            query = queryContent2
-        }
-        
-        if (dataType && isOwn) {
-            query = AV.Query.and(queryContent, queryContent2)
-        }
-        
-        if (limit) {
-            query.lessThanOrEqualTo('createdAt', limitCondition)
-            query.limit(limit)
-        }
-    
-        query.descending('createdAt')
-        return query.find()
-    },
-    
-    queryById (id) {
-        return new AV.Query('content').get(id)
-    },
-    
-    queryByArray (arr) {
-        arr = arr.map(id => '"' + id + '"')
-        let sql = 'select * from comment where contentId in (' + arr.join(',') + ')'
-        return AV.Query.doCloudQuery(sql)
-    },
-    
-    delContentById (id) {
-        const talk = AV.Object.createWithoutData('content', id)
-        return talk.destroy()
-    },
-    
-    delComment (id) {
-        const comment = AV.Object.createWithoutData('comment', id)
-        return comment.destroy()
-    },
-    
-    delCommentById (id) {
-        return new Promise((resolve, reject) => {
-            AV.Query.doCloudQuery('select * from comment where contentId="'+ id +'"').then(res => {
-                const Objects = res.results.map(item => {
-                    return AV.Query.doCloudQuery('delete from comment where objectId="'+ item.id +'"')
-                })
-        
-                AV.Object.saveAll(Objects).then(() => {
-                    resolve()
-                }).catch(() => {
-                    reject()
-                })
-            }).catch(() => {
-                reject()
-            })
-        })
+  user: null,
+
+  openid: 0,
+
+  setTableContent () {
+    const Content = AV.Object.extend('content')
+    return new Content()
+  },
+
+  setComment () {
+    const Comment = AV.Object.extend('comment')
+    return new Comment()
+  },
+
+  commonField () {
+    return {
+      user: this.user,
+      openid: this.openid
     }
+  },
+
+  getOneTalk (id) {
+    return AV.Object.createWithoutData('content', id)
+  },
+
+  queryBy (dataType, isOwn = true, limit, limitCondition) {
+    let queryContent
+    let queryContent2
+    let query
+
+    if (dataType) {
+      queryContent = new AV.Query('content')
+      queryContent.equalTo('dataType', dataType)
+      query = queryContent
+    }
+
+    if (isOwn) {
+      queryContent2 = new AV.Query('content')
+      queryContent2.equalTo('openid', this.openid)
+      query = queryContent2
+    }
+
+    if (dataType && isOwn) {
+      query = AV.Query.and(queryContent, queryContent2)
+    }
+
+    if (limit) {
+      query.lessThanOrEqualTo('createdAt', limitCondition)
+      query.limit(limit)
+    }
+
+    query.descending('createdAt')
+    return query.find()
+  },
+
+  queryById (id) {
+    return new AV.Query('content').get(id)
+  },
+
+  queryByArray (arr) {
+    arr = arr.map(id => '"' + id + '"')
+    let sql = 'select * from comment where contentId in (' + arr.join(',') + ')'
+    return AV.Query.doCloudQuery(sql)
+  },
+
+  queryCount(ids, table) {
+    let promise = []
+    ids.forEach(commentId=> {
+      let query = new AV.Query(table)
+      query.equalTo('contentId', commentId)
+      promise.push(query.count())
+    })
+
+    return Promise.all(promise)
+  },
+
+  delContentById (id) {
+    const talk = AV.Object.createWithoutData('content', id)
+    return talk.destroy()
+  },
+
+  delComment (id) {
+    const comment = AV.Object.createWithoutData('comment', id)
+    return comment.destroy()
+  },
+
+  delCommentById (id) {
+    return new Promise((resolve, reject) => {
+      AV.Query.doCloudQuery('select * from comment where contentId="'+ id +'"').then(res => {
+        const Objects = res.results.map(item => {
+          return AV.Query.doCloudQuery('delete from comment where objectId="'+ item.id +'"')
+        })
+
+        AV.Object.saveAll(Objects).then(() => {
+          resolve()
+        }).catch(() => {
+          reject()
+        })
+      }).catch(() => {
+        reject()
+      })
+    })
+  }
 }
