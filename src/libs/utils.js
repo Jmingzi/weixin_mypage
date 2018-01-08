31ffa0212ea2af92184d1f5be224b5876aeacf11
yangming <472102644@qq.com>
2018-01-08 18:42:26 +0800

    优化首页与card页bug

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -17,7 +17,7 @@ export default {
       })
     })
   },
-  
+
   loading (title = '加载中') {
     if (typeof title === 'boolean') {
       wx.hideLoading()
@@ -25,7 +25,7 @@ export default {
       wx.showLoading({ title })
     }
   },
-  
+
   modal (opt = {}) {
     return new Promise((resolve, reject) => {
       wx.showModal({
@@ -42,7 +42,7 @@ export default {
       })
     })
   },
-  
+
   downFile(url) {
     return new Promise((resolve, reject) => {
       wx.downloadFile({
@@ -53,7 +53,7 @@ export default {
       })
     })
   },
-  
+
   getLocalImg(path) {
     return new Promise((resolve, reject) => {
       wx.getImageInfo({
@@ -65,7 +65,7 @@ export default {
       })
     })
   },
-  
+
   drawCanvas(screenRect, detail, callback) {
     let width = screenRect.width - 20
     let height = screenRect.height
@@ -75,16 +75,16 @@ export default {
     let imgH = drawType === 1 ? 200 : height - 150
     let hasImg = Boolean(detail.talkImg[0])
     let utils = this
-  
+
     function _drawWorld(context, text) {
       let startX, startY, middleX, middleY
       let brNum = 0
       let colWidth = 20
       let rowWidth = 14
-    
+
       context.setFillStyle('black')
       context.setFontSize(14)
-    
+
       // 横向排列的文本需要手动计算宽度
       // 手动换行
       let newText = ''
@@ -97,21 +97,21 @@ export default {
         }
       }
       text = newText
-      
+
       // 得到换行后的文本
       text = text.split('\n')
       // 初始的x轴坐标是根据换行数来确定
       middleX = imgW / 2 + colWidth * (text.length + 1) / 2
-    
+
       text.forEach((textItem, colIndex)=> {
         brNum += 1
-        
+
         if (drawType === 1) {
           // 每一列起始的x轴坐标
           startX = middleX - brNum * colWidth
           // 每一列起始的y坐标
           middleY = hasImg ? (imgH + colWidth*3) : imgH / 2
-  
+
           if (colIndex === 0) {
             // 第一列加上小红圈
             context.arc(startX + 7, middleY - 5, 3, 0, 2 * Math.PI)
@@ -124,7 +124,7 @@ export default {
           middleX = 10
           startY = middleY = (hasImg ? (imgH + 20) : 100) + brNum * colWidth
         }
-      
+
         for (let i = 0; i < textItem.length; i++) {
           if (drawType === 1) {
             // 每一个文字的起始y坐标
@@ -132,25 +132,25 @@ export default {
           } else {
             startX = middleX + rowWidth*i
           }
-          
+
           context.fillText(textItem[i], startX, startY)
         }
       })
     }
-  
+
     function _drawAvatar(context) {
       let avatarW = 50
       let avatarX = 10
       let avatarXEnd = screenRect.width - 10 - avatarW
       let avatarY = screenRect.height
       let worldY = avatarY - avatarW / 2 - 5
-    
+
       context.setFontSize(10)
       context.setFillStyle('#aaaaaa')
-    
+
       // slogan
       // context.fillText('小心情', avatarXEnd - 40, worldY)
-    
+
       // logo
       context.drawImage(
         // detail.user.avatarUrl,
@@ -160,7 +160,7 @@ export default {
         avatarW,
         avatarW
       )
-      
+
       // avatar
       // context.drawImage(
       //   detail.user.avatarUrl,
@@ -177,7 +177,7 @@ export default {
         worldY + 20
       )
     }
-    
+
     function _saveToImage() {
       utils.loading('正在生成图片...')
       setTimeout(()=> {
@@ -200,27 +200,29 @@ export default {
         })
       }, 1000)
     }
-  
+
     context.setFillStyle('#fafafa')
     context.fillRect(0, 0, screenRect.width, height)
-  
+
     _drawWorld(context, detail.talkContent)
-  
+
     // 异步获取图片
     _drawAvatar(context)
-  
+
     if (hasImg) {
       this.modal({
         showCancel: true,
         content: '本应用使用的图片服务是http的，小程序不支持直接生成，需手动选择'
-      }).then(()=> {
-        wx.chooseImage({
-          success(res) {
-            context.drawImage(res.tempFilePaths[0], 10, 10, imgW, imgH)
-            context.draw()
-            _saveToImage()
-          }
-        })
+      }).then((res)=> {
+        if (res.confirm) {
+          wx.chooseImage({
+            success(res) {
+              context.drawImage(res.tempFilePaths[0], 10, 10, imgW, imgH)
+              context.draw()
+              _saveToImage()
+            }
+          })
+        }
       })
     } else {
       context.draw()
